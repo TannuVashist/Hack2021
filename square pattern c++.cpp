c3758293ca2adcdd4cd0b1917218e200f4668cde
Adarsh Soni <57979544+Adarsh9497@users.noreply.github.com>
2021-10-19 17:36:21 +0530

    Square pattern in C++

---
+++ square pattern c++.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main() 
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
